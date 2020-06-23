4fe7b8d17d12b7e3e3f222e28ce8875d68f75318
Kfir Matityahu <kfir25812@gmail.com>
2020-06-23 16:32:15 +0300

    fixed web library injection race state

--- lib/flutter_mongo_stitch_web.dart
+++ lib/flutter_mongo_stitch_web.dart
@@ -1,33 +1,38 @@
 library flutter_mongo_stitch_web;
 
-//import 'dart:collection';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-//import 'package:flutter/services.dart';
 import 'package:flutter_mongo_stitch_platform_interface/flutter_mongo_stitch_platform_interface.dart';
-import 'package:flutter_mongo_stitch_web/utils.dart';
-import 'implementation.dart';
+
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+import 'src/utils.dart';
+import 'src/interop.dart';
 
 /// The web implementation of [FlutterMongoStitchPlatform].
 ///
 /// This class implements the `package:flutter_mongo_stitch` functionality for the web.
 class FlutterMongoStitchPlugin extends FlutterMongoStitchPlatform {
-  FlutterMongoStitchPlugin(){
-    // Inject the desired libraries
-    injectJSLibraries([
-      "https://s3.amazonaws.com/stitch-sdks/js/bundles/4.9.0/stitch.js",
-      "https://fluttermongostitch.s3.us-east-2.amazonaws.com/stitchUtils.js"
-    ]);
-  }
+  MyMongoClient _mongoClient;
+  bool _injected = false;
 
-  static void registerWith(Registrar registrar) /*async*/ {
+  static void registerWith(Registrar registrar) async {
     // Registers this class as the default instance of [FlutterMongoStitchPlatform]
     FlutterMongoStitchPlatform.instance = FlutterMongoStitchPlugin();
   }
 
-  var _mongoClient = MyMongoClient();
+  Future<void> _init() async{
+    if(!_injected) {
+      // Inject the desired libraries
+      await injectJSLibraries([
+        "https://s3.amazonaws.com/stitch-sdks/js/bundles/4.9.0/stitch.js",
+        "https://fluttermongostitch.s3.us-east-2.amazonaws.com/stitchUtils.js"
+      ]);
+      _mongoClient = MyMongoClient();
+      _injected = true;
+    }
+  }
 
 
   @override
@@ -198,213 +203,10 @@ class FlutterMongoStitchPlugin extends FlutterMongoStitchPlatform {
         _mongoClient.setupWatchCollection(databaseName, collectionName, [ids, asObjectIds]);
       }
     }
-    else{
-
+    else {
       _mongoClient.setupWatchCollection(databaseName, collectionName, filter);
-
     }
 
     return Future.value(true);
   }
-
-//  @override
-//  aggregate({@required String collectionName, @required String databaseName,}){
-//    throw UnimplementedError('aggregate() has not been implemented.');
-//  }
-//
-//  _insertDocument(MethodCall call) async{
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final HashMap data = call.arguments["data"];
-//
-//    await _mongoClient.insertDocument(databaseName, collectionName, data);
-//    return true;
-//  }
-//
-//  _insertDocuments(MethodCall call) async{
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final List list = call.arguments["list"];
-//
-//    await _mongoClient.insertDocuments(databaseName, collectionName, list);
-//    return true;
-//  }
-//
-//  _deleteDocument(MethodCall call) async{
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final String filter = call.arguments['filter'];
-//
-//    String resultString =  await _mongoClient.deleteDocument(databaseName, collectionName, filter);
-//    Map<String, dynamic> map = json.decode(resultString);
-//
-//    return map["deletedCount"];
-//  }
-//
-//  _deleteDocuments(MethodCall call) async{
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final String filter = call.arguments['filter'];
-//
-//    String resultString =  await _mongoClient.deleteDocuments(databaseName, collectionName, filter);
-//    Map<String, dynamic> map = json.decode(resultString);
-//
-//    return map["deletedCount"];
-//  }
-//
-//  _findDocuments(MethodCall call) async {
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final String filter = call.arguments['filter'];
-//
-//    var list = await _mongoClient.findDocuments(databaseName, collectionName, filter);
-//    return list;
-//  }
-//
-//  _findDocument(MethodCall call) async{
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final String filter = call.arguments['filter'];
-//    //todo, ADD: final String projection = call.arguments['projection'];
-//
-//
-//    var list = await _mongoClient.findDocument(databaseName, collectionName, filter);
-//    return list;
-//
-//  }
-//
-//  _countDocuments(MethodCall call) async{
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final String filter = call.arguments['filter'];
-//
-//    var size = await _mongoClient.countDocuments(databaseName, collectionName, filter);
-//    return size;
-//  }
-//
-//  _updateDocument(MethodCall call) async{
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final String filter = call.arguments['filter'];
-//    final String update = call.arguments['update'];
-//
-//    String resultString = await _mongoClient.updateDocument(databaseName, collectionName, filter, update);
-//    Map<String, dynamic> map = json.decode(resultString);
-//
-//    return <int>[map["matchedCount"] , map["modifiedCount"]];
-//
-//  }
-//
-//  _updateDocuments(MethodCall call) async{
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final String filter = call.arguments['filter'];
-//    final String update = call.arguments['update'];
-//
-//    String resultString = await _mongoClient.updateDocuments(databaseName, collectionName, filter, update);
-//    Map<String, dynamic> map = json.decode(resultString);
-//
-//    return [map["matchedCount"] , map["modifiedCount"]];
-//  }
-//
-//  _aggregate(MethodCall call) async{}
-
-  ///====================================================================
-
-//  _signInAnonymously() async {
-//    var authResult = await _mongoClient.loginAnonymously();
-//    return authResult;
-//  }
-//
-//  _signInWithUsernamePassword(MethodCall call) async{
-//    final String username = call.arguments["username"];
-//    final String password = call.arguments["password"];
-//
-//
-//    var authResult =
-//    await _mongoClient.signInWithUsernamePassword(username, password);
-//    return authResult;
-//  }
-//
-//  _signInWithGoogle(MethodCall call) async{
-//    final String authCode = call.arguments["code"];
-//
-//    var authResult =
-//    await _mongoClient.signInWithGoogle(authCode);
-//    return authResult;
-//  }
-//
-//  _signInWithFacebook(MethodCall call) async{
-//
-//  }
-//
-//  _registerWithEmail(MethodCall call) async{
-//    final String email = call.arguments["email"];
-//    final String password = call.arguments["password"];
-//
-//    var authResult = await _mongoClient.registerWithEmail(email, password);
-//    return authResult;
-//  }
-//
-//  _logout() async{
-//    await _mongoClient.logout();
-//    return true;
-//  }
-//
-//  _getUserId() async{
-//    var id = await _mongoClient.getUserId();
-//    return id;
-//  }
-//
-//  _getUser() async{
-//    var authResult = await _mongoClient.getUser();
-//    return authResult;
-//  }
-//
-//  _sendResetPasswordEmail(MethodCall call) async{
-//    final String email = call.arguments["email"];
-//
-//    await _mongoClient.sendResetPasswordEmail(email);
-//    return true;
-//  }
-
-  ///====================================================================
-
-//  _callFunction(MethodCall call) async {
-//    final String name = call.arguments["name"];
-//    final List args = call.arguments["args"] ?? [];
-//    //final int timeout = call.arguments["timeout"] ?? 15000;
-//
-//    var result = _mongoClient.callFunction(name, args);//, timeout);
-//    return result;
-//  }
-
-  ///??///?
-
-//  _setupWatchCollection(MethodCall call){
-//    final String databaseName = call.arguments['database_name'];
-//    final String collectionName = call.arguments['collection_name'];
-//    final String filter = call.arguments['filter'];
-//    final List ids = call.arguments['ids'];
-//    final bool asObjectIds = call.arguments['as_object_ids'] ?? false;
-//
-//    print(call.arguments);
-//
-//
-//
-//    if(filter == null) {
-//      if (ids == null || ids.isEmpty) {
-//        _mongoClient.setupWatchCollection(databaseName, collectionName);
-//      }
-//      else {
-//        _mongoClient.setupWatchCollection(databaseName, collectionName, [ids, asObjectIds]);
-//      }
-//    }
-//    else{
-//
-//      _mongoClient.setupWatchCollection(databaseName, collectionName, filter);
-//
-//    }
-//  }
 }
-
